b072c9c0ba45b5a6b92d712edcf8797cd2046350
samuel maveke <mavekepeter2022@gmail.com>
2025-02-09 15:14:49 +0300

    deploying to render

---
+++ backend/routes/cart.route.js
@@ -0,0 +1,12 @@
+import express from "express"
+import {addToCart, getAllproducts, removeAllFromCart, updatequantity} from '../controller/cart.controller.js'
+import { protectRoute } from "../middleware/auth.middleware.js"
+
+const router = express.Router()
+
+router.get("/",protectRoute,getAllproducts)
+router.post("/",protectRoute,addToCart)
+router.delete("/",protectRoute,removeAllFromCart)
+router.put("/:id",protectRoute,updatequantity)
+
+export default router
